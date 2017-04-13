2b23126d71a8c9afb0ca69bf185bee69d8b91954
Paul Torres <torrepaul@gmail.com>
2017-04-13 15:49:50 -0600

    everything good on the front end except validations need to be better.....now to reddit clone part 2

---
+++ Modules/app.config.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular.module('app').config(config)
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
+
+  function config($stateProvider, $urlRouterProvider, $locationProvider){
+
+
+    $locationProvider.html5Mode(true)
+
+    $stateProvider
+      .state({
+        name: 'home',
+        url: '/',
+        component: 'app',
+      })
+  }
+
+}());
